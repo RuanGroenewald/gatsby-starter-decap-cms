812316021b2d54cf2707d288c976661923060fbd
RuanGroenewald <77604907+RuanGroenewald@users.noreply.github.com>
2024-05-31 15:24:57 +0200

    added image gallery

---
+++ src/pages/image-gallery/image-gallery.md
@@ -0,0 +1,15 @@
+---
+templateKey: image-gallery
+path: /image-gallery
+title: Image Gallery
+date: 2024-05-31T00:00:00.000Z
+featuredimage: /img/gallery.jpg
+description: A collection of beautiful images.
+images:
+  - image: /img/sample1.jpg
+    alt: Sample Image 1
+  - image: /img/sample2.jpg
+    alt: Sample Image 2
+  - image: /img/sample3.jpg
+    alt: Sample Image 3
+---
---
+++ src/templates/image-gallery.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
+
+const ImageGalleryTemplate = ({ data }) => {
+  const { markdownRemark: post } = data;
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  return (
+    <Layout>
+      <h1>{post.frontmatter.title}</h1>
+      <p>{post.frontmatter.description}</p>
+      <div className="gallery">
+        {post.frontmatter.images.map((img, index) => (
+          <div key={index} className="thumbnail">
+            <img src={img.image} alt={img.alt} onClick={() => setSelectedImage(img.image)} />
+          </div>
+        ))}
+      </div>
+      {selectedImage && (
+        <div className="lightbox" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="" />
+        </div>
+      )}
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query ImageGalleryByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        description
+        images {
+          image
+          alt
+        }
+      }
+    }
+  }
+`;
+
+export default ImageGalleryTemplate;
