e0ffaadb3229ef90c33e140d943c10a35839f7a1
RuanGroenewald <77604907+RuanGroenewald@users.noreply.github.com>
2024-05-31 15:35:39 +0200

    changed image gallery js

--- src/pages/image-gallery/image-gallery.js
+++ src/pages/image-gallery/image-gallery.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import Layout from '../components/layout';
 
 const ImageGalleryTemplate = ({ data }) => {
