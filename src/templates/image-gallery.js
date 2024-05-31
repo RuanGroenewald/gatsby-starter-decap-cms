93b58e6284d4466b47e746fe39cc3f9ccad7ed4f
RuanGroenewald <77604907+RuanGroenewald@users.noreply.github.com>
2024-05-31 15:46:29 +0200

    changed layout to Layout

--- src/templates/image-gallery.js
+++ src/templates/image-gallery.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { graphql } from 'gatsby';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 
 const ImageGalleryTemplate = ({ data }) => {
   const { markdownRemark: post } = data;
